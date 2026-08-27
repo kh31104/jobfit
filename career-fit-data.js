3a075b35788e09ff9a02475e9493eb737867d6f7
kh31104 <kh31104@gmail.com>
2026-08-27 19:59:17 +0900

    Load Step 2 peer strengths improvements

--- career-fit-data.js
+++ career-fit-data.js
@@ -1,1 +1,1 @@
-document.write('<script src="career-fit-data-base.js"><\/script><script src="supabase-votes.js"><\/script>');
+document.write('<script src="career-fit-data-base.js"><\/script><script src="supabase-votes.js"><\/script><script src="step2-peer-strengths.js"><\/script>');
