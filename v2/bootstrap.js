e22bd0784f1b3c6a8af817c2143cb058fd9dfb84
김가휘 <kh31104@gmail.com>
2026-09-20 20:47:26 +0900

    Refresh Jobfit classroom modules

--- v2/bootstrap.js
+++ v2/bootstrap.js
@@ -20,8 +20,8 @@ if(isInjeCourse){
   }
 }
 
-await import('./app.js?v=15');
-await import('./injeClassroom.js?v=16');
+await import('./app.js?v=16');
+await import('./injeClassroom.js?v=17');
 await import('./careerDnaUx.js?v=7');
 await import('./careerDnaStudentUx.js?v=1');
 await import('./careerDnaLearningFlowUx.js?v=4');
