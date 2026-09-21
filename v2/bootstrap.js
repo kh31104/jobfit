4bcf1a9b597c0d67093e538909e9d2160b1bb718
김가휘 <kh31104@gmail.com>
2026-09-21 11:39:47 +0900

    Refresh app and STEP 2 UX cache versions

--- v2/bootstrap.js
+++ v2/bootstrap.js
@@ -20,11 +20,11 @@ if(isInjeCourse){
   }
 }
 
-await import('./app.js?v=20');
+await import('./app.js?v=21');
 await import('./injeClassroom.js?v=17');
 await import('./careerDnaUx.js?v=7');
 await import('./careerDnaStudentUx.js?v=1');
-await import('./careerDnaLearningFlowUx.js?v=5');
+await import('./careerDnaLearningFlowUx.js?v=6');
 const {startOperationalSync}=await import('./operationalSync.js?v=1');
 startContinuity();
 startOperationalSync();
